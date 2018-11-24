8133f1afe9c00792eb9f4cf26eb2e27a59ca7531
Edward d'Albon <edward.dalbon@gmail.com>
2018-11-24 18:31:23 +0000

    .extend() first version

--- src/Model.js
+++ src/Model.js
@@ -1,70 +1,149 @@
 
-export default class Model {
-  static spec = undefined
-  __attrs = null
+// export default class Model {
+//   static spec = undefined
+//   __attrs = null
 
 
-  constructor(attrs) {
-    if (attrs) {
-      this.spec = attrs
-    } else {
-      this.spec = this.constructor.attrs
-    }
-    this.init()
-  }
+//   constructor(attrs) {
+//     if (attrs) {
+//       this.spec = attrs
+//     } else {
+//       this.spec = this.constructor.attrs
+//     }
+//     this.init()
+//   }
 
-  init() {
-    this.__attrs = {}
+//   init() {
+//     this.__attrs = {}
 
-    for (let [key, val] of Object.entries(this.spec)) {
-      if (val.constructor.name === 'Array') {
-        let proxy = new Proxy(val, {
-          get: (target, name) => {
-            return target[name]
-          },
-          set: (target, name, value) => {
-            target[name] = value
+//     for (let [key, val] of Object.entries(this.spec)) {
+//       if (val.constructor.name === 'Array') {
+//         let proxy = new Proxy(val, {
+//           get: (target, name) => {
+//             return target[name]
+//           },
+//           set: (target, name, value) => {
+//             target[name] = value
 
-            // trigger change event
-            if (this.onChange !== undefined) {
-              this.onChange()
-            }
+//             // trigger change event
+//             if (this.onChange !== undefined) {
+//               this.onChange()
+//             }
 
-            return true
-          }
-        })
-        val = proxy
+//             return true
+//           }
+//         })
+//         val = proxy
 
-        // Object.defineProperty(this, key, {
-        //   set: val => {
-        //     this.__attrs[key] = val
+//         // Object.defineProperty(this, key, {
+//         //   set: val => {
+//         //     this.__attrs[key] = val
   
-        //     // trigger change event
-        //     if (this.onChange !== undefined) {
-        //       this.onChange()
-        //     }
-        //   },
-        //   get: () => {
-        //     return this.__attrs[key]
-        //   }
-        // })
-        // this[key] = proxy
-      }
+//         //     // trigger change event
+//         //     if (this.onChange !== undefined) {
+//         //       this.onChange()
+//         //     }
+//         //   },
+//         //   get: () => {
+//         //     return this.__attrs[key]
+//         //   }
+//         // })
+//         // this[key] = proxy
+//       }
 
-      Object.defineProperty(this, key, {
-        set: val => {
-          this.__attrs[key] = val
+//       Object.defineProperty(this, key, {
+//         set: val => {
+//           this.__attrs[key] = val
+
+//           // trigger change event
+//           if (this.onChange !== undefined) {
+//             this.onChange()
+//           }
+//         },
+//         get: () => {
+//           return this.__attrs[key]
+//         }
+//       })
+//       this[key] = val
+//     }
+//   }
+// }
+
+const Model = {}
+
+const Observable = (inst) => {
+  for (let [key, val] of Object.entries(inst.__props)) {
+    if (val.constructor.name === 'Array') {
+      console.log('array')
+
+      let proxy = new Proxy(val, {
+        get: (target, name) => {
+          return target[name]
+        },
+        set: (target, name, value) => {
+          target[name] = value
+
+          console.log('set arr')
 
           // trigger change event
-          if (this.onChange !== undefined) {
-            this.onChange()
+          if (inst.onChange !== undefined) {
+            inst.onChange()
           }
+
+          return true
         },
-        get: () => {
-          return this.__attrs[key]
-        }
+        __jawbone_meta_isproxy: true
       })
-      this[key] = val
+    
+      val = proxy
     }
+
+    console.log('val:', val)
+    // inst.__props[key] = val
+
+    Object.defineProperty(inst, key, {
+      set: val => {
+        inst.__props[key] = val
+
+        console.log('set')
+        // trigger change event
+        if (inst.onChange !== undefined) {
+          inst.onChange()
+        }
+      },
+      get: () => {
+        console.log('get:')
+        console.log('inst.__props[key].constructor:', inst.__props[key].constructor)
+        console.log('inst.__props[key].constructor.name:', inst.__props[key].constructor.name)
+        console.log('inst.__props[key]:', inst.__props[key])
+        
+        
+        if (inst.__props[key].constructor.name === 'Proxy') {
+          console.log('proxy')
+        }
+
+        return inst.__props[key]
+      }
+    })
+    inst[key] = val
   }
 }
+
+// extend
+Object.defineProperty(Model, 'extend', {
+  configurable: false,
+  enumerable: false,
+  writable: false,
+
+  value: (opts) => {
+    let inst = {}
+
+    inst.__props = opts.props
+
+    Observable(inst)
+
+    return inst    
+  }
+})
+
+export default Model
--- test/App.js
+++ test/App.js
@@ -1,24 +1,143 @@
 import Jawbone from '../src/Jawbone'
 
-class CatView extends Jawbone.View {
-  static template = m => {
-    return `
-      <p>Hi, I'm a cat and my name is: ${m.name} </p>`
-  }
-}
+// class CatView extends Jawbone.View {
+//   static template = m => {
+//     return `
+//       <p>Hi, I'm a cat and my name is: ${m.name} </p>`
+//   }
+// }
 
-class HelloView extends Jawbone.View {
-  static el = '#root'
-  static template = (m) => {
-    return `
-      <input type="text" value="${m.name}">
-      <p>Hello, ${m.name} </p>
-      <p>A list, ${m.list} </p>`
-  }
-}
+// class HelloView extends Jawbone.View {
+//   static el = '#root'
+//   static template = (m) => {
+//     return `
+//       <input type="text" value="${m.name}">
+//       <p>Hello, ${m.name} </p>
+//       <p>A list, ${m.list} </p>`
+//   }
+// }
 
-let hello = { name: 'World', list: [1, 2, 3] }
-let helloView = new HelloView(hello)
-// note that you have to reassign hello to use it as a model
-hello = helloView.model
-hello.list.push(1)
+// let hello = { name: 'World', list: [1, 2, 3] }
+// let helloView = new HelloView(hello)
+// // note that you have to reassign hello to use it as a model
+// hello = helloView.model
+// hello.list.push(1)
+
+// class Foo {
+//   attrs = {}
+//   events = {}
+//   something() {
+//     console.log('this.attrs:', this.attrs)
+//     console.log('this.lol:', this.lol)
+    
+    
+
+//   }
+//   constructor(attrs) {
+//     // let child = new Object.create(Object.getPrototypeOf(this.constructor))
+//     // console.log('child:', child)
+//     this.attrs = null    
+//     Object.defineProperty(this, 'lol', { value: 'kek'})
+//   }
+// }
+
+// class Bar extends Foo {
+//   attrs = {
+//     x: 12,
+//     lol: this.something,
+//   }
+//   defaultProp = 15
+
+// }
+
+
+// let bar = new Bar()
+// bar.something()
+
+let mod = Jawbone.Model.extend({
+  props: {
+    name: 'John',
+    names: ['Jenny', 'Johnny']
+  }
+})
+window.mod = mod
+
+// class Model {
+//   static spec = undefined
+//   __attrs = null
+
+
+//   constructor(attrs) {
+//     if (attrs) {
+//       this.spec = attrs
+//     } else {
+//       this.spec = this.constructor.attrs
+//     }
+//     this.init()
+//   }
+
+//   init() {
+//     this.__attrs = {}
+
+//     for (let [key, val] of Object.entries(this.spec)) {
+//       if (val.constructor.name === 'Array') {
+//         let proxy = new Proxy(val, {
+//           get: (target, name) => {
+//             return target[name]
+//           },
+//           set: (target, name, value) => {
+//             target[name] = value
+
+//             // trigger change event
+//             if (this.onChange !== undefined) {
+//               this.onChange()
+//             }
+
+//             return true
+//           }
+//         })
+//         val = proxy
+
+//         // Object.defineProperty(this, key, {
+//         //   set: val => {
+//         //     this.__attrs[key] = val
+  
+//         //     // trigger change event
+//         //     if (this.onChange !== undefined) {
+//         //       this.onChange()
+//         //     }
+//         //   },
+//         //   get: () => {
+//         //     return this.__attrs[key]
+//         //   }
+//         // })
+//         // this[key] = proxy
+//       }
+
+//       Object.defineProperty(this, key, {
+//         set: val => {
+//           this.__attrs[key] = val
+
+//           // trigger change event
+//           if (this.onChange !== undefined) {
+//             this.onChange()
+//           }
+//         },
+//         get: () => {
+//           return this.__attrs[key]
+//         }
+//       })
+//       this[key] = val
+//     }
+//   }
+// }
+
+// class Dog extends Model {
+//   static attrs = {
+//     name: 'John',
+//     names: ['The', 'Dog']
+//   }
+// }
+
+// let dog = new Dog();
+// window.dog = dog
